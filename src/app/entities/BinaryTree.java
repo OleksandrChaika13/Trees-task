4e2bf6fa9bc6ff5f78d18d8ffc50b5f462bb958b
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-04 10:38:52 +0300

    Creating "BinaryTree" class

--- src/app/entities/BinaryTree.java
+++ src/app/entities/BinaryTree.java
@@ -1,4 +1,34 @@
 package app.entities;
 
 public class BinaryTree {
+
+    public Node root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    public void traverseInOrder(Node node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.key);
+            traverseInOrder(node.right);
+        }
+    }
+
+    public void traversePreOrder(Node node) {
+        if (node != null) {
+            System.out.print(" " + node.key);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    public void traversePostOrder(Node node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.key);
+        }
+    }
 }

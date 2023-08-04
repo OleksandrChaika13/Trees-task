fc63cc8e0f47ded5d9dcff6c74a67272a6c8aed2
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-04 10:45:25 +0300

    Fixing "Main" class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,10 @@
 package app;
 
+import app.utils.AppStarter;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My binary trees app");
+        AppStarter.starter();
     }
 }

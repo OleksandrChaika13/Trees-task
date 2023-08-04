2c68ca3d3ef57d39018524b0c2840ea32127c2a0
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-04 10:41:54 +0300

    Creating "Outputer" class

--- src/app/utils/Outputer.java
+++ src/app/utils/Outputer.java
@@ -1,4 +1,24 @@
 package app.utils;
 
+import app.entities.BinaryTree;
+
 public class Outputer {
+
+    public static void printInOrder(BinaryTree tree) {
+        System.out.print("Inorder traversal: ");
+        tree.traverseInOrder(tree.root);
+        System.out.println();
+    }
+
+    public static void printPreOrder(BinaryTree tree) {
+        System.out.print("Preorder traversal: ");
+        tree.traversePreOrder(tree.root);
+        System.out.println();
+    }
+
+    public static void printPostOrder(BinaryTree tree) {
+        System.out.print("Postorder traversal: ");
+        tree.traversePostOrder(tree.root);
+        System.out.println();
+    }
 }

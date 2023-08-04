49b65b46f2e6b04fe9a9d4ec8e4b81b38800ac8b
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-04 10:37:56 +0300

    Creating "Node" class

--- src/app/entities/Node.java
+++ src/app/entities/Node.java
@@ -1,3 +1,14 @@
 package app.entities;
+
 public class Node {
+
+    public int key;
+    public app.entities.Node left;
+    public app.entities.Node right;
+
+    public Node(int item) {
+        key = item;
+        left = null;
+        right = null;
+    }
 }
